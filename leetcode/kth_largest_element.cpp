9f46d8343aa3cbfa2580d148ad08e7bfe8925b70
Patrick Demers <patrickdemers6@gmail.com>
2022-09-28 15:27:31 -0500

    add kth_largest_element

---
+++ leetcode/kth_largest_element.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/kth-largest-element-in-an-array
+
+#include <vector>
+#include <queue>
+using namespace std;
+
+int findKthLargest(vector<int> &nums, int k)
+{
+    priority_queue<int, vector<int>, greater<>> pq;
+    for (int n : nums)
+    {
+        pq.emplace(n);
+        if (pq.size() > k)
+            pq.pop();
+    }
+    return pq.top();
+}
