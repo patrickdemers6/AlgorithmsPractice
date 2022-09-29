789e1495ffb4ed2da3cebe9ff18c5e8550227145
Patrick Demers <patrickdemers6@gmail.com>
2022-09-29 16:34:27 -0500

    solve leetcode removing_minimum_and_maximum_from_array

---
+++ leetcode/removing_minimum_and_maximum_from_array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define feo(a) for (int i = 0; i < a.size(); ++i)
+#define fei(b) for (int j = 0; j < b.size(); ++j)
+#define fe(a) feo(a) fei(a[0])
+
+class Solution {
+   public:
+    int minimumDeletions(vector<int>& nums) {
+        int low(0), high(0);
+        feo(nums) {
+            if (nums[i] < nums[low])
+                low = i;
+            else if (nums[i] > nums[high])
+                high = i;
+        }
+        if (low > high) swap(low, high);
+        // ->low high<-, ->high, low<-
+        return min(min(low + ((int)nums.size() - 1 - high) + 2, high + 1), (int)nums.size() - low);
+    }
+};
