d62cfa9bdffbaa4ab1011b46eeb2f03e5b9f9f4d
Patrick Demers <patrickdemers6@gmail.com>
2022-10-12 23:53:35 -0500

    add template for cses

---
+++ cses/template.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <vector>
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define EVEN(a) !(a & 1)
+#define ODD(a) a & 1
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+using namespace std;
+int main() {
+    return 0;
+}
